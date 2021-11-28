6d27b2ea12e33cb26b442a39eb9d136cdb97f92a
sebpaj <sseba07@gmail.com>
2021-11-28 21:14:07 +0100

    federation schema update, setting apollo client, adding login mutation on front end:

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ﻿# Getting Started
+
 Let's start
+
+# Generating schema federation
+
+rover supergraph compose --config ./supergraph-config.yaml
+rover supergraph compose --config ./supergraph-config.yaml > supergraph.graphql
+Convert to UTF-8 with Notepad++
--- frontend/src/components/log-in.tsx
+++ frontend/src/components/log-in.tsx
@@ -3,6 +3,7 @@ import GoogleLogin, {
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
 } from "react-google-login";
+import { gql, useMutation } from "@apollo/client";
 
 export interface LoginProps {
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
@@ -13,17 +14,35 @@ export interface LoginProps {
 }
 
 const clientId: string = process.env.REACT_APP_GOOGLE_CLIENT_ID ?? "";
+const LOGIN_MUTATION = gql`
+  mutation Login($input: LoginMutationInput!) {
+    login(input: $input) {
+      success
+    }
+  }
+`;
 
 export default function Login(props: LoginProps): JSX.Element {
   const { setIsLoggedIn, setToken, setName, setEmail, setImageUrl } = props;
+  const [login, { data, loading, error }] = useMutation(LOGIN_MUTATION);
   const onSuccess = (res: any) => {
-    console.log("res below");
-    console.log(res);
     setIsLoggedIn(true);
     setToken(res.tokenId);
     setName(res.profileObj.name);
     setEmail(res.profileObj.email);
     setImageUrl(res.profileObj.imageUrl);
+
+    console.log("user logged, calling mutation");
+    login({
+      variables: {
+        input: {
+          firstName: res.profileObj.name,
+          email: res.profileObj.email,
+          imageUrl: res.profileObj.imageUrl,
+        },
+      },
+    });
+    console.log("data", data, "loading", loading, "error", error);
   };
 
   const onFailure = (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
